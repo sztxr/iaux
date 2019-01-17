942ba74d7a365edd8157e7c4575b7445ecb83885
Mitra Ardron <mitra@mitra.biz>
2019-01-17 14:54:45 +1100

    Modify related-as-list to use fix some bugs, catch errors and use IAReactComponent

--- packages/ia-components/sandbox/related-as-list.js
+++ packages/ia-components/sandbox/related-as-list.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import IAReactComponent from './IAReactComponent.js';
 import PropTypes from 'prop-types'
+import TileImage from './tile-image.js';
 
 import { RelatedService } from 'ia-js-client'
 
 // TODO separate the data fetching from the presentation
 
-export default class extends React.Component {
+export default class extends IAReactComponent {
 
   static propTypes = {
     identifier: PropTypes.string.isRequired
@@ -17,29 +18,40 @@ export default class extends React.Component {
       related: []
     }
 
-    const relatedService = new RelatedService()
-    relatedService.get({identifier: this.props.identifier}).then((results) => {
-      this.setState({related: results.hits.hits})
-    })
+    const relatedService = new RelatedService();
+
+    relatedService.get({identifier: this.props.identifier})
+        .then((related) => {
+          this.setState({related: related.data.hits.hits});   // TODO-DWEB make this an array of Member and allow TileImage to take member=Member
+        })
+        .catch((err) => {
+          debug("Related service failed err= %s", err.message);
+          this.setState({related: []});
+        });
   }
 
   render () {
+    if (!this.state.related) {
+      return <div>Related items loading...</div>
+    }
+
     const horizontalScrollStyling = {
       width: '100%',
       overflowX: 'scroll',
       margin: 'auto',
-      whiteSpace: 'nowrap'
-    }
+    }; //whiteSpace: 'nowrap' makes text overlap - let Richard fix this
+    const paraStyling = { whiteSpace: 'wrap'};
     let children = this.state.related.map((row) => {
-      return <li style={{display: 'inline-block', margin: '1rem', maxWidth: 200}}>
+      //TODO-DWEB needs to add item to img
+      return <li style={{display: 'inline-block', margin: '1rem', maxWidth: 200}} key={row._id} >
         <a href={`?identifier=${row._id}`}>
-          <img src={`https://archive.org/services/img/${row._id}`} />
+          <TileImage identifier={row._id} />
           <p>{ row._source.title[0] }</p>
         </a>
       </li>
     })
     if (children.length === 0) {
-      children.push(<li>[related]</li>)
+      children.push(<li key="RELATED">[related]</li>)
     }
     return <div>
       <h2>Related</h2>
